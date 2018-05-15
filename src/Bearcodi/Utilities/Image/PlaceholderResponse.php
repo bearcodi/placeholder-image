90d8d9c5cd236773626973289c5e29fdfb2ba4e9
Mark <mark@bearcodi.com>
2018-05-15 05:49:29 +0000

    Apply fixes from StyleCI

--- src/Bearcodi/Utilities/Image/PlaceholderResponse.php
+++ src/Bearcodi/Utilities/Image/PlaceholderResponse.php
@@ -2,18 +2,14 @@
 
 namespace Bearcodi\Utilities\Image;
 
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-
 class PlaceholderResponse
 {
     public static function make()
     {
         return new Placeholder();
     }
-    
+
     protected function getRequestParams()
     {
-        
     }
 }
